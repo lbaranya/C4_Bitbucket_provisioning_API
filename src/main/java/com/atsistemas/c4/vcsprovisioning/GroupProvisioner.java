4760ae1c270d2706b61fdaec725af285b90001f6
lbaranya <lbaranya@atsistemas.com>
2018-04-17 14:40:47 +0200

    se añade manejo de errores

--- src/main/java/com/atsistemas/c4/vcsprovisioning/GroupProvisioner.java
+++ src/main/java/com/atsistemas/c4/vcsprovisioning/GroupProvisioner.java
@@ -1,20 +1,19 @@
 package com.atsistemas.c4.vcsprovisioning;
 
-
 import com.atlassian.stash.rest.client.core.http.UriBuilder;
-import static com.atlassian.stash.rest.client.core.http.HttpMethod.GET;
 import static com.atlassian.stash.rest.client.core.http.HttpMethod.POST;
 
 import com.atlassian.stash.rest.client.core.StashClientImpl;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.atlassian.stash.rest.client.core.http.HttpExecutor;
-import com.atlassian.stash.rest.client.core.http.HttpMethod;
-import com.atlassian.stash.rest.client.httpclient.HttpClientHttpExecutor;
-import com.atlassian.stash.rest.client.httpclient.HttpClientConfig;
+
 import com.atlassian.stash.rest.client.api.StashRestException;
 
 public class GroupProvisioner extends StashClientImpl {
+	
+	public static final int GROUP_ARREADY_EXISTS = 409;
+	public static final int GROUP_CREATED = 200;
+	public static final int ADMIN_PRIVILEGES_REQUIRED = 401;
+	public static final int MALFORMED_REQUEST = 400;
 
 
 	public GroupProvisioner(HttpExecutor httpExecutor) {
@@ -23,11 +22,10 @@ public class GroupProvisioner extends StashClientImpl {
 
 	}
 
-	public void addGroup(String groupName) {
+	public void addGroup(String groupName) throws Exception{
 		
             UriBuilder uriBuilder = UriBuilder.forPath("/rest/api/1.0/admin/groups")
             		.addQueryParam("name", groupName);
-            System.out.println("uriBuilder: " + uriBuilder);
             try {
             	
             	super.doRestCall(uriBuilder, POST, null, false);
@@ -35,22 +33,23 @@ public class GroupProvisioner extends StashClientImpl {
             	
             } catch (StashRestException e) {
             	
-            	//TODO cambiar manejo excepción	
-                System.out.println("el código del error" + e.getStatusCode());
-                throw e;
+                int status = e.getStatusCode();
+                
+                switch (status) {
+                
+                	case GROUP_ARREADY_EXISTS:
+                		break;
+                	case ADMIN_PRIVILEGES_REQUIRED: 
+                		throw new Exception("Admin Privileges required for this operation");
+                	case MALFORMED_REQUEST:
+                		throw new Exception("Malformed request");
+                	case GROUP_CREATED:
+                		break;
+                	default:
+                		throw new Exception("Unknown Error");
+                }
             	
             }
         }
-	
-	
-	public boolean groupExists (String groupName) {
-		
-		
-		
-		
-		//Cambiar el retorno del método
-		return true;
-		
-	}
 	
 }
